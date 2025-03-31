1f9e0aa90e81feefff506ae0fd9236fd3895b963
Priti Modanwal <pritivns612@gmail.com>
2025-03-31 23:29:16 +0530

    wedwed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.html", "./src/**/*.js"],
+  content: ["./index.html", "./src/**/*.html", "./src/**/*.js"],   
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+
+// Here * mean add any extension file
